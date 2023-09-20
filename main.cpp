6010ced72af0f3c5334144cbc38fee90ac883f42
adrgonza <adrian.gonser@icloud.com>
2023-09-20 18:19:51 +0200

    added args

--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dangonza <dangonza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adrgonza <adrgonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 13:11:51 by dangonza          #+#    #+#             */
-/*   Updated: 2023/07/27 15:49:23 by dangonza         ###   ########.fr       */
+/*   Updated: 2023/09/20 18:14:46 by adrgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <Server.hpp>
 
-int main(void)
-{
+int main(int argc, char **argv) {
+
+	if (argc != 3) {
+		std::cout << "\033[0;31mERROR: input structure expected:\033[0m ./ircserv <port> <password>" << std::endl;
+		return (1);
+	}
+
+	(void)argv;
 	Server server = Server();
 	try
 	{
