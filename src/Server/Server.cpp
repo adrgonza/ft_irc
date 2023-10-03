47143ea808de7918f22c4488b53e20a5dc779bff
adrgonza <adrian.gonser@icloud.com>
2023-10-03 20:25:33 +0200

    disconnect client improved

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -89,19 +89,19 @@ bool Server::handleClientCommunications(size_t i)
 	if (readSize == 0)
 	{
 		std::cout << "[SERVER]: A Client was disconnected from the server" << std::endl;
-		std::vector<Client>::iterator it = std::find(_clients.begin(), _clients.end(), _pollFd[i].fd);
-		close(_pollFd[i].fd);
-		if (it != _clients.end())
+		Client *delet = findClientByFd(_pollFd[i].fd);
+		close(delet->getFd());
+		for (std::vector<Client>::iterator it = _clients.begin(); it != _clients.end(); it++)
 		{
-			_disconnectedClients.push_back(*it);
-			_clients.erase(it);
+			if (it->getFd() == delet->getFd())
+			{
+				_disconnectedClients.push_back(*delet);
+				_clients.erase(it);
+				break;
+			}
 		}
-		for (size_t i = 0; i < BACKLOG; i++)
-		{
-			if (_pollFd[i + 1].fd == _pollFd[i].fd)
-				_pollFd[i + 1].fd = -1;
-			_pollFd[i].revents = 0;
-		}
+		_pollFd[i].fd = -1;
+		_pollFd[i].revents = 0;
 	}
 	else
 	{
